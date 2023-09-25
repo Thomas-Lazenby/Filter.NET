2dad3101e229ad5dbcbfcbdd26c3f410c8179ecc
Thomas Lazenby <41332369+Thomas-Lazenby@users.noreply.github.com>
2023-09-25 14:09:47 +0100

    Organised IFilter more and added more methods.

--- src/IFilter.cs
+++ src/IFilter.cs
@@ -3,43 +3,40 @@
 namespace Filter
 {
     /// <summary>
-    /// Interface for a filtering mechanism for collections of items of type T. Filter's are <see cref="Filter{T}"/> and ConcurrentFilter
+    /// Interface for a filtering mechanism for collections of items of type T.
+    /// <para>Keywords</para>
+    /// <para>Explicitly - Registered manually using Include and Exclude methods, overriding the <see cref="Default"/> behaviour.</para>
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public interface IFilter<T> : ICloneable, IEquatable<IFilter<T>>
         where T : notnull, IEquatable<T>
     {
         #region Write Operations
+
         /// <summary> The default filter option. </summary>
         FilterType Default { get; set; }
 
-        /// <summary> Overrides the current default behavior for the specified item, marking it to be included in the filter results. </summary>
+        /// <summary> Overrides the current filter behavior for the specified <paramref name="item"/>, marking it to be included explicitly in the filter results. </summary>
         /// <param name="item">The item to include in the filter results.</param>
         /// <returns>The filter itself.</returns>
         IFilter<T> Include(T item);
 
-        /// <summary> Overrides the current default behavior for the specified items, marking them to be included in the filter results. </summary>
+        /// <summary> Overrides the current filter behavior for <paramref name="items"/>, marking it to be included explicitly in the filter results. </summary>
         /// <param name="items">The items to include in the filter results.</param>
         /// <returns>The filter itself.</returns>
         IFilter<T> Include(params T[] items);
 
-        /// <summary>
-        /// Overrides the current default behavior for the specified item, marking it to be excluded from the filter results.
-        /// </summary>
+        /// <summary> Overrides the current filter behavior for the specified <paramref name="item"/>, marking it to be excluded explicitly in the filter results. </summary>
         /// <param name="item">The item to exclude from the filter results.</param>
         /// <returns>The filter itself.</returns>
         IFilter<T> Exclude(T item);
 
-        /// <summary>
-        /// Overrides the current default behaviours for the specified items, marking them to be excluded from the filter results.
-        /// </summary>
+        /// <summary> Overrides the current filter behavior for <paramref name="items"/>, marking it to be excluded explicitly in the filter results. </summary>
         /// <param name="items">The items to exclude from the filter results.</param>
         /// <returns>The filter itself.</returns>
         IFilter<T> Exclude(params T[] items);
 
-        /// <summary>
-        /// Removes the specified item from the override list, causing it to revert to the default filter behavior during filtering operations.
-        /// </summary>
+        /// <summary> Removes the specified item from the override list, causing it to revert to the default filter behavior during filtering operations. </summary>
         /// <param name="item">The item to reset to default filter behavior.</param>
         /// <returns>The filter itself.</returns>
         IFilter<T> SetAsDefault(T item);
@@ -51,6 +48,8 @@ namespace Filter
         #endregion
 
         #region Read Operations
+
+        #region Explicit
 
         /// <summary>
         /// Retrieves a list of items that are explicitly set to be included.
@@ -65,33 +64,68 @@ namespace Filter
         IEnumerable<T> ExplicitExcludedItems { get; }
 
         /// <summary>
-        /// Determines if the specified item is explicitly excluded based on the current filter settings.
+        /// Determines if the specified item is explicitly excluded ( <see cref="Exclude(T)"/>, <see cref="Exclude(T[])"/> ) based on the current filter settings.
         /// </summary>
         /// <param name="item">The item to check.</param>
         /// <returns>true if the item is explicitly excluded; otherwise, false.</returns>
         bool IsExplicitlyExcluded(T item);
 
         /// <summary>
-        /// Determines if the specified item is explicitly included based on the current filter settings.
+        /// Determines if the specified item is explicitly included ( <see cref="Include(T)"/>, <see cref="Include(T[])"/> ) based on the current filter settings.
         /// </summary>
         /// <param name="item">The item to check.</param>
         /// <returns>true if the item is explicitly included; otherwise, false.</returns>
         bool IsExplicitlyIncluded(T item);
 
+        /// <summary>
+        /// Determines if any of the items are explicitly included ( <see cref="Include(T)"/> or <see cref="Include(T[])"/> ) if so will return true.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        bool AnyExplicitIncluded(params T[] items);
 
+        /// <summary>
+        /// Determines if any of the items are explicitly excluded ( <see cref="Exclude(T)"/> or <see cref="Exclude(T[])"/> ) if so will return true.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        bool AnyExplicitExcluded(params T[] items);
+
+        #endregion
+
+        /// <summary> Determines if the specified item is included either by being explititly included ( <see cref="Include(T)"/>, <see cref="Include(T[])"/> ) or by default ( <see cref="Default"/> ) </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         bool IsIncluded(T item);
 
+        /// <summary> Determines if the specified item is included either by being explititly excluded ( <see cref="Exclude(T)"/>, <see cref="Exclude(T[])"/> ) or by default ( <see cref="Default"/> ) </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         bool IsExcluded(T item);
 
-
-        bool ShouldPass(params T[] items);
-
-        bool ShouldFail(params T[] items);
-        
-
+        /// <summary> Any values that should be included either by <see cref="Default"/> or ( <see cref="Include(T)"/>, <see cref="Include(T[])"/> ) </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
         bool AnyIncluded(params T[] items);
 
+        /// <summary> Any values that should be excluded either by <see cref="Default"/> or ( <see cref="Exclude(T)"/>, <see cref="Exclude(T[])"/> ) </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
         bool AnyExcluded(params T[] items);
+
+        /// <summary>
+        /// Will use <see cref="Default"/> and <see cref="ExplicitExcludedItems"/> if any are contained in either then will return false; else true.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        bool ShouldPass(params T[] items);
+
+        /// <summary>
+        /// Will use <see cref="Default"/> and <see cref="ExplicitIncludedItems"/> if any are contained in either then will return true; else false.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        bool ShouldFail(params T[] items);
 
         #endregion
     }
